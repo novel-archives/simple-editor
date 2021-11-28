490426ad65715a5167f3540327e41ffa3ad97454
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2021-11-28 23:47:09 +0900

    feat: implemented simple-editor

--- crates/simple-editor/dist/index-6692e17a4c070642.js
+++ crates/simple-editor/dist/index-6692e17a4c070642.js
@@ -18,6 +18,22 @@ function addHeapObject(obj) {
     return idx;
 }
 
+let cachedTextDecoder = new TextDecoder('utf-8', { ignoreBOM: true, fatal: true });
+
+cachedTextDecoder.decode();
+
+let cachegetUint8Memory0 = null;
+function getUint8Memory0() {
+    if (cachegetUint8Memory0 === null || cachegetUint8Memory0.buffer !== wasm.memory.buffer) {
+        cachegetUint8Memory0 = new Uint8Array(wasm.memory.buffer);
+    }
+    return cachegetUint8Memory0;
+}
+
+function getStringFromWasm0(ptr, len) {
+    return cachedTextDecoder.decode(getUint8Memory0().subarray(ptr, ptr + len));
+}
+
 function dropObject(idx) {
     if (idx < 36) return;
     heap[idx] = heap_next;
@@ -97,14 +113,6 @@ function debugString(val) {
 
 let WASM_VECTOR_LEN = 0;
 
-let cachegetUint8Memory0 = null;
-function getUint8Memory0() {
-    if (cachegetUint8Memory0 === null || cachegetUint8Memory0.buffer !== wasm.memory.buffer) {
-        cachegetUint8Memory0 = new Uint8Array(wasm.memory.buffer);
-    }
-    return cachegetUint8Memory0;
-}
-
 let cachedTextEncoder = new TextEncoder('utf-8');
 
 const encodeString = (typeof cachedTextEncoder.encodeInto === 'function'
@@ -166,13 +174,49 @@ function getInt32Memory0() {
     return cachegetInt32Memory0;
 }
 
-let cachedTextDecoder = new TextDecoder('utf-8', { ignoreBOM: true, fatal: true });
+function makeMutClosure(arg0, arg1, dtor, f) {
+    const state = { a: arg0, b: arg1, cnt: 1, dtor };
+    const real = (...args) => {
+        // First up with a closure we increment the internal reference
+        // count. This ensures that the Rust closure environment won't
+        // be deallocated while we're invoking it.
+        state.cnt++;
+        const a = state.a;
+        state.a = 0;
+        try {
+            return f(a, state.b, ...args);
+        } finally {
+            if (--state.cnt === 0) {
+                wasm.__wbindgen_export_2.get(state.dtor)(a, state.b);
 
-cachedTextDecoder.decode();
+            } else {
+                state.a = a;
+            }
+        }
+    };
+    real.original = state;
 
-function getStringFromWasm0(ptr, len) {
-    return cachedTextDecoder.decode(getUint8Memory0().subarray(ptr, ptr + len));
+    return real;
 }
+function __wbg_adapter_16(arg0, arg1) {
+    wasm._dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hdf274bb7bc586d09(arg0, arg1);
+}
+
+let stack_pointer = 32;
+
+function addBorrowedObject(obj) {
+    if (stack_pointer == 1) throw new Error('out of js stack');
+    heap[--stack_pointer] = obj;
+    return stack_pointer;
+}
+function __wbg_adapter_19(arg0, arg1, arg2) {
+    try {
+        wasm._dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hd796fb25e52819aa(arg0, arg1, addBorrowedObject(arg2));
+    } finally {
+        heap[stack_pointer++] = undefined;
+    }
+}
+
 /**
 */
 export function run_app() {
@@ -224,7 +268,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-d8311ace67f13981_bg.wasm', import.meta.url);
+        input = new URL('index-6692e17a4c070642_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
@@ -235,6 +279,10 @@ async function init(input) {
     imports.wbg.__wbindgen_is_undefined = function(arg0) {
         var ret = getObject(arg0) === undefined;
         return ret;
+    };
+    imports.wbg.__wbindgen_string_new = function(arg0, arg1) {
+        var ret = getStringFromWasm0(arg0, arg1);
+        return addHeapObject(ret);
     };
     imports.wbg.__wbindgen_cb_drop = function(arg0) {
         const obj = takeObject(arg0).original;
@@ -248,6 +296,14 @@ async function init(input) {
     imports.wbg.__wbindgen_object_drop_ref = function(arg0) {
         takeObject(arg0);
     };
+    imports.wbg.__wbg_Window_f826a1dec163bacb = function(arg0) {
+        var ret = getObject(arg0).Window;
+        return addHeapObject(ret);
+    };
+    imports.wbg.__wbg_WorkerGlobalScope_967d186155183d38 = function(arg0) {
+        var ret = getObject(arg0).WorkerGlobalScope;
+        return addHeapObject(ret);
+    };
     imports.wbg.__wbg_instanceof_Window_c4b70662a0d2c5ec = function(arg0) {
         var ret = getObject(arg0) instanceof Window;
         return ret;
@@ -256,6 +312,13 @@ async function init(input) {
         var ret = getObject(arg0).document;
         return isLikeNone(ret) ? 0 : addHeapObject(ret);
     };
+    imports.wbg.__wbg_clearTimeout_2c1ba0016d8bca41 = function(arg0, arg1) {
+        getObject(arg0).clearTimeout(arg1);
+    };
+    imports.wbg.__wbg_setTimeout_df66d951b1726b78 = function() { return handleError(function (arg0, arg1, arg2) {
+        var ret = getObject(arg0).setTimeout(getObject(arg1), arg2);
+        return ret;
+    }, arguments) };
     imports.wbg.__wbg_createElement_86c152812a141a62 = function() { return handleError(function (arg0, arg1, arg2) {
         var ret = getObject(arg0).createElement(getStringFromWasm0(arg1, arg2));
         return addHeapObject(ret);
@@ -313,6 +376,9 @@ async function init(input) {
     imports.wbg.__wbg_setvalue_2459f62386b6967f = function(arg0, arg1, arg2) {
         getObject(arg0).value = getStringFromWasm0(arg1, arg2);
     };
+    imports.wbg.__wbg_addEventListener_09e11fbf8b4b719b = function() { return handleError(function (arg0, arg1, arg2, arg3, arg4) {
+        getObject(arg0).addEventListener(getStringFromWasm0(arg1, arg2), getObject(arg3), getObject(arg4));
+    }, arguments) };
     imports.wbg.__wbg_removeEventListener_24d5a7c12c3f3c39 = function() { return handleError(function (arg0, arg1, arg2, arg3, arg4) {
         getObject(arg0).removeEventListener(getStringFromWasm0(arg1, arg2), getObject(arg3), arg4 !== 0);
     }, arguments) };
@@ -329,12 +395,26 @@ async function init(input) {
     imports.wbg.__wbg_setAttribute_1b533bf07966de55 = function() { return handleError(function (arg0, arg1, arg2, arg3, arg4) {
         getObject(arg0).setAttribute(getStringFromWasm0(arg1, arg2), getStringFromWasm0(arg3, arg4));
     }, arguments) };
+    imports.wbg.__wbg_clearTimeout_c1f246d6874c0679 = function(arg0, arg1) {
+        getObject(arg0).clearTimeout(arg1);
+    };
+    imports.wbg.__wbg_setTimeout_5314850184d61a44 = function() { return handleError(function (arg0, arg1, arg2) {
+        var ret = getObject(arg0).setTimeout(getObject(arg1), arg2);
+        return ret;
+    }, arguments) };
     imports.wbg.__wbg_lastChild_ca5bac177ef353f6 = function(arg0) {
         var ret = getObject(arg0).lastChild;
         return isLikeNone(ret) ? 0 : addHeapObject(ret);
     };
     imports.wbg.__wbg_setnodeValue_702374ad3d0ec3df = function(arg0, arg1, arg2) {
         getObject(arg0).nodeValue = arg1 === 0 ? undefined : getStringFromWasm0(arg1, arg2);
+    };
+    imports.wbg.__wbg_textContent_eef491bffc43d8d6 = function(arg0, arg1) {
+        var ret = getObject(arg1).textContent;
+        var ptr0 = isLikeNone(ret) ? 0 : passStringToWasm0(ret, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
+        var len0 = WASM_VECTOR_LEN;
+        getInt32Memory0()[arg0 / 4 + 1] = len0;
+        getInt32Memory0()[arg0 / 4 + 0] = ptr0;
     };
     imports.wbg.__wbg_appendChild_d318db34c4559916 = function() { return handleError(function (arg0, arg1) {
         var ret = getObject(arg0).appendChild(getObject(arg1));
@@ -360,6 +440,10 @@ async function init(input) {
         var ret = Object.is(getObject(arg0), getObject(arg1));
         return ret;
     };
+    imports.wbg.__wbg_new_0b83d3df67ecb33e = function() {
+        var ret = new Object();
+        return addHeapObject(ret);
+    };
     imports.wbg.__wbg_globalThis_3f735a5746d41fbd = function() { return handleError(function () {
         var ret = globalThis.globalThis;
         return addHeapObject(ret);
@@ -376,6 +460,10 @@ async function init(input) {
         var ret = global.global;
         return addHeapObject(ret);
     }, arguments) };
+    imports.wbg.__wbg_set_82a4e8a85e31ac42 = function() { return handleError(function (arg0, arg1, arg2) {
+        var ret = Reflect.set(getObject(arg0), getObject(arg1), getObject(arg2));
+        return ret;
+    }, arguments) };
     imports.wbg.__wbindgen_debug_string = function(arg0, arg1) {
         var ret = debugString(getObject(arg1));
         var ptr0 = passStringToWasm0(ret, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
@@ -385,6 +473,14 @@ async function init(input) {
     };
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
+    };
+    imports.wbg.__wbindgen_closure_wrapper1331 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 49, __wbg_adapter_16);
+        return addHeapObject(ret);
+    };
+    imports.wbg.__wbindgen_closure_wrapper1333 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 51, __wbg_adapter_19);
+        return addHeapObject(ret);
     };
 
     if (typeof input === 'string' || (typeof Request === 'function' && input instanceof Request) || (typeof URL === 'function' && input instanceof URL)) {
--- crates/simple-editor/src/lib.rs
+++ crates/simple-editor/src/lib.rs
@@ -1,23 +1,133 @@
+use novel_archives_text::parser::token::{ParsedSpan, ParsedToken};
+use std::collections::BTreeMap;
+use std::sync::Arc;
+use std::time::Duration;
+use yew::virtual_dom::VNode;
+
+use novel_archives_text::parser::token;
 use wasm_bindgen::prelude::*;
 use yew::prelude::*;
+use yew::services::timeout::*;
 
 struct Model {
     link: ComponentLink<Self>,
+    version: usize,
+    viewer_text: String,
+    viewer_nodes: Vec<VNode>,
+    change_text_timeout: Option<TimeoutTask>,
+    ctx: token::ParseContext,
 }
 
 enum Msg {
-    AddOne,
+    ChangeText(String),
+    ParseRequestText(usize),
+}
+
+impl Model {
+    fn view_token(&self, token: ParsedToken) -> VNode {
+        match token {
+            ParsedToken::Plaintext(span) => self.view_plaintext(span),
+            ParsedToken::Space(span) => self.view_space(span),
+            ParsedToken::NewLine(span) => self.view_newline(span),
+            ParsedToken::Ignore(_) => {
+                panic!("unexpected token")
+            }
+            ParsedToken::Term { body, term_id } => self.view_term(body, term_id),
+            ParsedToken::Ruby { body, ruby } => self.view_ruby(body, ruby),
+            ParsedToken::KanjiRuby { body, ruby } => self.view_ruby(body, ruby),
+            ParsedToken::Annotation { body, description } => {
+                self.view_annotation(body, description)
+            }
+        }
+    }
+    fn view_plaintext(&self, span: ParsedSpan) -> VNode {
+        html! {
+            <span>{span.fragment()}</span>
+        }
+    }
+    fn view_space(&self, span: ParsedSpan) -> VNode {
+        html! {
+            <span>{span.fragment()}</span>
+        }
+    }
+    fn view_newline(&self, _: ParsedSpan) -> VNode {
+        html! {
+            <br/>
+        }
+    }
+    fn view_term(
+        &self,
+        body: ParsedSpan,
+        _term_id: novel_archives_text::Id<novel_archives_text::term::Term>,
+    ) -> VNode {
+        html! {
+            <a>{body.fragment()}</a>
+        }
+    }
+    fn view_ruby(&self, body: ParsedSpan, ruby: ParsedSpan) -> VNode {
+        html! {
+            <ruby>
+                <rb>{body.fragment()}</rb>
+                <rp>{"("}</rp>
+                <rt>{ruby.fragment()}</rt>
+                <rp>{")"}</rp>
+            </ruby>
+        }
+    }
+    fn view_annotation(
+        &self,
+        body: ParsedSpan,
+        _description: token::iterator::TextIterator,
+    ) -> VNode {
+        html! {
+            <a>{body.fragment()}</a>
+        }
+    }
 }
 
 impl Component for Model {
     type Message = Msg;
     type Properties = ();
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Self { link }
+        Self {
+            link,
+            version: 0,
+            viewer_text: String::new(),
+            viewer_nodes: vec![],
+            change_text_timeout: None,
+            ctx: token::ParseContext::new(Arc::new(BTreeMap::new())),
+        }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
-        true
+        match msg {
+            Msg::ChangeText(text) => {
+                let (version, _) = self.version.overflowing_add(1);
+                self.version = version;
+                self.viewer_text = text;
+                self.change_text_timeout = Some(TimeoutService::spawn(
+                    Duration::from_secs(1),
+                    self.link.callback(move |_| Msg::ParseRequestText(version)),
+                ));
+                false
+            }
+            Msg::ParseRequestText(version) => {
+                if self.version == version {
+                    let iter = token::iterator::TextIterator::new(
+                        self.ctx.clone(),
+                        token::ParsedSpan::new(&self.viewer_text),
+                    );
+                    let nodes: Vec<_> = iter
+                        .filter(|token| !matches!(token, ParsedToken::Ignore(_)))
+                        .map(|token| self.view_token(token))
+                        .collect();
+                    self.viewer_nodes = nodes;
+                    true
+                } else {
+                    false
+                }
+            }
+        }
     }
 
     fn change(&mut self, _props: Self::Properties) -> ShouldRender {
@@ -28,10 +138,10 @@ impl Component for Model {
         html! {
             <div class="simple-editor">
                 <div class="simple-editor__editor">
-                    < p>{"editor"}</p>
+                    <textarea class="simple-editor__editor__textarea"  oninput={self.link.callback(|e:InputData| Msg::ChangeText(e.value))} />
                 </div>
                 <div class="simple-editor__viewer">
-                    <p>{"viewer"}</p>
+                    {self.viewer_nodes.clone()}
                 </div>
             </div>
         }
